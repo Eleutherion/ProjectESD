94ec250a37e6cc703d031dd57228e5b441f60658
Lucky Leo Ambal <ambal.luckyleo@gmail.com>
2018-10-07 21:24:52 +0800

    Changes

--- ProjectESD/ModInterpolate.vb
+++
@@ -1,9 +0,0 @@
-﻿Module ModInterpolate
-    Public Function Interpolate(CeilingMark As Decimal, FloorMark As Decimal, CeilingValue As Decimal, FloorValue As Decimal, RequiredMark As Decimal) As Decimal
-        Dim reqCorVal As Decimal
-
-        reqCorVal = ((CeilingValue - FloorValue) * (RequiredMark - FloorMark) / (CeilingMark - FloorMark)) + FloorValue
-
-        Return reqCorVal
-    End Function
-End Module
