47dda8e5521ca031e6fba0561dcb25a41c0e9196
Lucky Leo Ambal <ambal.luckyleo@gmail.com>
2018-11-02 00:37:39 +0800

    Add Splash Screen

--- ProjectESD/My Project/Application.Designer.vb
+++ ProjectESD/My Project/Application.Designer.vb
@@ -34,5 +34,10 @@ Namespace My
         Protected Overrides Sub OnCreateMainForm()
             Me.MainForm = Global.ProjectESD.FormMain
         End Sub
+        
+        <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
+        Protected Overrides Sub OnCreateSplashScreen()
+            Me.SplashScreen = Global.ProjectESD.FormSplash
+        End Sub
     End Class
 End Namespace
